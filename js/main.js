05a6c40aa2d1de269c792294e3a6aee0ed8dd6f1
David <david.baron@softtek.com>
2016-05-27 22:01:07 -0500

    added elements to practice

--- js/main.js
+++ js/main.js
@@ -13,4 +13,26 @@ $( document ).ready(function() {
 	function onWhiteClick() {
 		$("#theme").attr("href","../css/main2.css");
 	}
+	$('.carousel').carousel({
+  		interval: 10000
+	})
+	new WOW().init();
+	$("#showhide").click(function () {
+      $("#mapa").each(function() {
+        displaying = $(this).css("display");
+        if(displaying == "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","block");
+          });
+        }
+      });
+    });
+	$("#logo").click(content);
+	function content() {
+		$(".adding").append('<img src="../img/logo.png" alt="">')
+	}
 });
